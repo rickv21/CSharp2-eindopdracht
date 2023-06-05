97f5167f00d24c010c1f007e4d9b71b7fb05eec6
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2023-06-05 22:45:38 +0200

    set up board front end

---
+++ gameproject/GameProject/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class Cell
+    {
+        public Color CellColor { get; set; }
+        public Color PieceColor { get; set; }
+    }
+}
