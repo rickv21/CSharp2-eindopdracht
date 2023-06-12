7f8dc3aa97dc5e39e3a62f45adcb58f08879273a
Cedric Smit <cedsmit@gmail.com>
2023-06-12 03:01:13 +0200

    Added initial class that will handle all the networking code.

---
+++ gameproject/GameProject/Networking/Network.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Networking
+{
+    internal class Network
+    {
+        private readonly int PORT = 5732;
+
+        private char PlayerChar;
+        private char OpponentChar;
+        private Socket sock;
+        private TcpListener server = null;
+        private TcpClient client;
+        private BackgroundWorker MessageReceiver = new BackgroundWorker();
+
+        public Network(bool isHost, string ip = null)
+        {
+
+            if (isHost)
+            {
+                Debug.WriteLine("Host has been created!");
+
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine("Player B has connected!");
+                    client = new TcpClient(ip, this.PORT);
+                    sock = client.Client;
+                    MessageReceiver.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
